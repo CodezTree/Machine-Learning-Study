fdf47cf647fc1a062ff059a04c6dba2c7bc89909
Changjun Mun <codeztree@naver.com>
2019-02-01 20:26:29 +0900

    Relu
    add Relu

--- DeepLearningFromScratch-Study/Layers.py
+++ DeepLearningFromScratch-Study/Layers.py
@@ -16,11 +16,27 @@ class Affine:
         return out
 
     def backward(self, dout):
-        dx = np.dot(dout, self.W.T)
+        dx = np.dot(dout, self.W.T) # 역전파 흐름에 가중치 전치행렬을 dot product -> 이해 안되면 계산 그래프 그려 이해
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
 
         return dx
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0) # 0 보다 작은 인덱스를 마스킹
+        out = x.copy()
+        out[self.mask] = 0 # 복사한 out에서 0보다 작은 원소 값을 0으로 만듬
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+
+        return dout
 
 class SoftmaxWithLoss:
     def __init__(self):
@@ -39,4 +55,4 @@ class SoftmaxWithLoss:
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size
 
-        return dx
+        return dx * dout
